239c974b30fe1f20ba68572bd283e7b4dac2ba84
onekiguwolf <onekiguwolf@gmail.com>
2025-01-17 15:52:28 +0800

    Make the first Lab (50% done)

--- routes/cscpe-handler.js
+++ routes/cscpe-handler.js
@@ -8,6 +8,14 @@ router.get('/01', function(req, res, next) {
   res.render('labs/CSCPE/cscpe-01/home');
 });
 
+router.get('/01/lab01/aim', function(req, res, next){
+  res.render('labs/CSCPE/cscpe-01/lab-01/aim')
+})
+
+router.get('/01/lab01/theory', function(req, res, next){
+  res.render('labs/CSCPE/cscpe-01/lab-01/theory')
+})
+
 
 
 module.exports = router;
