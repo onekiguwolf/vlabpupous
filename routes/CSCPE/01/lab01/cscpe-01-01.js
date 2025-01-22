a0e24f50e8f250dfe23a7e72235b541fc027da6b
onekiguwolf <onekiguwolf@gmail.com>
2025-01-23 04:24:41 +0800

    finished Lab 01 for ANN

--- routes/CSCPE/01/lab01/cscpe-01-01.js
+++ routes/CSCPE/01/lab01/cscpe-01-01.js
@@ -19,4 +19,12 @@ router.get('/observations', function(req, res, next){
   res.render('labs/CSCPE/cscpe-01/lab-01/observations')
 })
 
+router.get('/assignment', function(req, res, next){
+  res.render('labs/CSCPE/cscpe-01/lab-01/assignment')
+})
+
+router.get('/references', function(req, res, next){
+  res.render('labs/CSCPE/cscpe-01/lab-01/references')
+})
+
 module.exports = router;
