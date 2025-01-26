a7b7aa85d1bf3eb13287ab6dcd4848857c1a4f34
onekiguwolf <onekiguwolf@gmail.com>
2025-01-27 06:38:39 +0800

    fixing annetworks.js router

--- routes/CSCPE/01/annetworks.js
+++ routes/CSCPE/01/annetworks.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* Pages for different Home Pages -- CSCPE*/
 router.get('/', function(req, res, next){
-  res.render('labs/CSCPE/cscpe-01/home')
+  res.render('../../../labs/CSCPE/cscpe-01/home')
 })
 router.get('/lab01', function(req, res, next) {
-  res.render('labs/CSCPE/cscpe-01/lab-01/aim');
+  res.render('../../../labs/CSCPE/cscpe-01/lab01/aim');
 });
 router.get('/lab02', function(req, res, next) {
-  res.render('labs/CSCPE/cscpe-01/lab-02/aim');
+  res.render('../../../labs/CSCPE/cscpe-01/lab02/aim');
 });
 router.get('/lab03', function(req, res, next) {
-  res.render('labs/CSCPE/cscpe-01/lab-03/aim');
+  res.render('../../../labs/CSCPE/cscpe-01/lab03/aim');
 });
 router.get('/lab04', function(req, res, next) {
-  res.render('labs/CSCPE/cscpe-01/lab-04/aim');
+  res.render('../../../labs/CSCPE/cscpe-01/lab04/aim');
 });
 
 module.exports = router;
